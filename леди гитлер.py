62c97648d52079e07d03b0ea6d3a2461976191ec
student <superzarub@mail.ru>
2024-04-28 15:08:17 +0500

    ботик

--- леди гитлер.py
+++ леди гитлер.py
@@ -1,9 +1,1 @@
-import logging
-from telegram import Update
-from telegram.ext import Updater
-
-TOKEN = "7060030383:AAHPgrlOgGXBxXL31_N1ztmli_yYsz1SSQE"
-
-logging.basicConfig( format='%(asctime)s - %(name)s
-
 
