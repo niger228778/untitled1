025a51e3c620e0b7df7b7136033e072e974dc286
student <superzarub@mail.ru>
2024-05-05 15:43:08 +0500

    649644+84

--- TELEGRAM BOT .py
+++ TELEGRAM BOT .py
@@ -41,7 +41,7 @@ async def cmd_test1(message: types.Message):
 
 @dp.message(Command("help"))
 async def cmd_test2(message: types.Message):
-    await message.reply("Все доступные команды в данном боте: /help, /Hello, /You_are_my_sunshine, /gay, /telegram_piton_govno")
+    await message.reply("Все доступные команды в данном боте: /films, /videos, /wikipedia, /help, /Hello, /You_are_my_sunshine, /gay, /telegram_piton_govno")
 
 @dp.message(Command(commands =["gay"]))
 async def cmd_test3(message: types.Message):
@@ -54,6 +54,17 @@ async def cmd_test4(message: types.Message):
 @dp.message(Command(commands =["You_are_my_sunshine"]))
 async def cmd_test5(message: types.Message):
     await bot.send_photo(chat_id=message.from_user.id, photo=FSInputFile('./леброн.webp'), caption="My only sunshine...")
+
+@dp.message(Command(commands =["films"]))
+async def cmd_test3(message: types.Message):
+    await message.reply("хз зачем тебе ссылка на рандомные фильмы но [вот](http://castlots.org/sluchajnyj-film/)", parse_mode="Markdown", disable_web_page_preview=True)
+
+@dp.message(Command(commands =["videos"]))
+async def cmd_test3(message: types.Message):
+    await message.reply("случайное видео на ютубе? [ну ок](https://www.youtube-tech.ru/random-pick/random-video/)", parse_mode="Markdown", disable_web_page_preview=True)
+@dp.message(Command(commands =["wikipedia"]))
+async def cmd_test3(message: types.Message):
+    await message.reply("рандомная статья на википедии? [легко](https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0%A1%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0)", parse_mode="Markdown", disable_web_page_preview=True)
 
 if __name__ == "__main__":
     asyncio.run(main())
