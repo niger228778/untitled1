ccd09eb803926dda0ddb167fb280047c348b35f3
student <superzarub@mail.ru>
2024-05-12 15:54:06 +0500

    сигма буданов

--- TELEGRAM BOT .py
+++ TELEGRAM BOT .py
@@ -35,13 +35,13 @@ def handler (message):
     links[message.text](message)
 
 
-@dp.message(Command(commands =["Hello"]))
+@dp.message(Command(commands =["hello"]))
 async def cmd_test1(message: types.Message):
-    await message.reply("Папа привет, сделай сделай пожалуйста эмоцию сигмы слово пацана!!!")
+    await message.reply("Папа привет, сделай пожалуйста эмоцию сигмы слово пацана!!!")
 
 @dp.message(Command("help"))
 async def cmd_test2(message: types.Message):
-    await message.reply("Все доступные команды в данном боте: /films, /videos, /wikipedia, /help, /Hello, /You_are_my_sunshine, /gay, /telegram_piton_govno")
+    await message.reply("Все доступные команды в данном боте: /xz, /random_number, /random, /toilet, /films, /videos, /wikipedia, /help, /hello, /you_are_my_sunshine, /gay, /telegram_piton_govno")
 
 @dp.message(Command(commands =["gay"]))
 async def cmd_test3(message: types.Message):
@@ -51,7 +51,7 @@ async def cmd_test3(message: types.Message):
 async def cmd_test4(message: types.Message):
     await message.reply("нажми [здесь](https://t.me/+lgnzFvBJBYZhM2Fi)", parse_mode="Markdown", disable_web_page_preview=True)
 
-@dp.message(Command(commands =["You_are_my_sunshine"]))
+@dp.message(Command(commands =["you_are_my_sunshine"]))
 async def cmd_test5(message: types.Message):
     await bot.send_photo(chat_id=message.from_user.id, photo=FSInputFile('./леброн.webp'), caption="My only sunshine...")
 
@@ -65,6 +65,26 @@ async def cmd_test3(message: types.Message):
 @dp.message(Command(commands =["wikipedia"]))
 async def cmd_test3(message: types.Message):
     await message.reply("рандомная статья на википедии? [легко](https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0%A1%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0)", parse_mode="Markdown", disable_web_page_preview=True)
+
+@dp.message(Command(commands =["xz"]))
+async def cmd_test3(message: types.Message):
+    await message.reply("ti xotel skazat KZ?)))")
+
+@dp.message(Command(commands =["random_number"]))
+async def cmd_test3(message: types.Message):
+    await message.reply("рандомное число от 1 до 100? Хм наверно [52](https://www.random.org/)", parse_mode="Markdown", disable_web_page_preview=True)
+
+@dp.message(Command(commands =["random"]))
+async def cmd_test3(message: types.Message):
+    await message.reply("бро итс джаст рандомный [генератор](https://randomall.ru/custom/gen/random?id=5098)", parse_mode="Markdown", disable_web_page_preview=True)
+
+@dp.message(Command(commands =["toilet"]))
+async def cmd_test3(message: types.Message):
+    await message.reply("[какой ты скебеде тоалет?](https://randomall.ru/custom/gen/9727)", parse_mode="Markdown", disable_web_page_preview=True)
+
+@dp.message()
+async def cmd_test3(message: types.Message):
+    await message.reply("я не знаю такую команду")
 
 if __name__ == "__main__":
     asyncio.run(main())
